0541f02f328b9fcb33cff08468ad008e9766cbe0
darbumas <2314@holbertonschool.com>
2021-05-21 16:46:57 -0500

    syntax missing a comma?

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,13 @@
 # Client configuration with Puppet
-file_line { 'Private key configuration ``~/.ssh/holberton``':
-	ensure 	=> 'present'
-	path 	=> '/etc/ssh/ssh_config',
-	line 	=> '	IdentityFile ~/.ssh/holberton'
+
+file_line { 'Private key configuration ~/.ssh/holberton':
+  ensure => 'present',
+  path => '/etc/ssh/ssh_config',
+  line => '	IdentityFile ~/.ssh/holberton'
 }
 
 file_line { 'Refuse auth with password':
-	ensure 	=> 'present',
-	path 	=> '/etc/ssh/ssh_config',
-	line 	=> '	PasswordAuthentication no',
+  ensure => 'present',
+  path => '/etc/ssh/ssh_config',
+  line => '	PasswordAuthentication no',
 }
